4f5420fdef37bbc159ea24d26292a83de3eca461
Anuj Sharma <anshrma@amazon.com>
2022-05-23 16:03:03 -0700

    working version

--- bin/sample-kong-apps.ts
+++ bin/sample-kong-apps.ts
@@ -11,8 +11,12 @@ const app = new cdk.App();
 const devEnv = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region:  process.env.CDK_DEFAULT_REGION,
+  licese_secret_name : 'kong-license-cdk',
 };
-const kong_control_plane_eks = new KongCpEks(app, 'kong-cp-eks', { env: devEnv });
+const kong_control_plane_eks = new KongCpEks(app, 'kong-cp-eks', { 
+  env: devEnv,
+  licese_secret_name: devEnv.licese_secret_name, 
+});
 const kong_control_plane_ecs = new KongCpEcs(app, 'kong-cp-ecs', { env: devEnv });
 
 new KongDpEks(app, 'kong-dp-eks', {
@@ -21,6 +25,8 @@ new KongDpEks(app, 'kong-dp-eks', {
   vpc: kong_control_plane_eks.control_plane.vpc,
   telemetry_dns: kong_control_plane_eks.telemetry_dns,
   private_ca_arn: kong_control_plane_eks.private_ca_arn,
+  licese_secret_name: devEnv.licese_secret_name,
+  prometheus_endpoint: 'https://aps-workspaces.us-east-1.amazonaws.com/workspaces/ws-b9eedac3-ae00-41dd-9812-2d7f3238bdb0/api/v1/remote_write',
 });
 
 new KongDpEcs(app, 'kong-dp-ecs', {
@@ -30,4 +36,13 @@ new KongDpEcs(app, 'kong-dp-ecs', {
   telemetry_dns: kong_control_plane_ecs.telemetry_dns,
   private_ca_arn: kong_control_plane_ecs.private_ca_arn,
 });
+
+new KongDpEcs(app, 'kong-dp-ecs-with-eks-cp', {
+  env: devEnv,
+  cluster_dns: kong_control_plane_eks.cluster_dns,
+  vpc: kong_control_plane_eks.control_plane.vpc,
+  telemetry_dns: kong_control_plane_eks.telemetry_dns,
+  private_ca_arn: kong_control_plane_eks.private_ca_arn,
+});
+
 
